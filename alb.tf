be27c8ee705859fdd7927de4f2dd3e48c878a863
Shaukat Ali <70197872+Samfurious275@users.noreply.github.com>
2025-08-28 01:14:38 +0500

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -13,7 +13,7 @@ resource "aws_lb" "app" {
 
 # Target Group
 resource "aws_lb_target_group" "main" {
-  name     = "web-tg"
+  name     = "web-tg-${github.run_id}"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
