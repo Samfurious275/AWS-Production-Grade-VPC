22c536e8ecc9bc523fbbc2e888b9691362baf624
Shaukat Ali <70197872+Samfurious275@users.noreply.github.com>
2025-08-28 00:28:05 +0500

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bastion" {
-  ami                    = "ami-0abcdef1234567890" # Amazon Linux 2
+  ami                    = "ami-0e95a5e2743ec9ec9" # Amazon Linux 2
   instance_type          = var.instance_type
   key_name               = var.key_name
   subnet_id              = aws_subnet.public[0].id
